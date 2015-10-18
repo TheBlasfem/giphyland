d775859b10c569609370334ba151af9637ec75c7
Julio Lopez <ljuliom@gmail.com>
2015-10-18 10:56:38 -0300

    displaying gifs with hbs

--- src/js/page/index.js
+++ src/js/page/index.js
@@ -1,9 +1,16 @@
 var giphy = require('./giphy');
+var photosEl = document.querySelector('.photos');
+var utils = require('./utils');
+var photosTemplate = require('./views/photos.hbs');
 
-
-getGifsData('cats').then(function(data){
-  console.log(data);
+//Initial Load
+var liveDataFetched = getGifsData('cats').then(function(result) {
+  if (!result) return false;
+  console.log(result.data[0]);
+  updatePage(result.data);
+  return true;
 });
+
 
 function getGifsData(searchTerm) {
   return giphy.search(searchTerm, {
@@ -11,4 +18,8 @@ function getGifsData(searchTerm) {
   }).catch(function(){
     return null;
   });
+}
+
+function updatePage(data) {
+  photosEl.insertBefore(utils.strToEls(photosTemplate(data)), photosEl.firstChild);
 }

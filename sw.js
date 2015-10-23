334ba985d7933c8139e820b10bdfc8bf04d58801
Julio Lopez <ljuliom@gmail.com>
2015-10-23 09:24:56 -0300

    changes

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"./src/js/sw/index.js":[function(require,module,exports){
 require('serviceworker-cache-polyfill');
-var CACHE_NAME = 'giphyland-v2';
+var CACHE_NAME = 'giphyland-v1';
 
 var urlsToCache = [
   './',
@@ -16,60 +16,6 @@ self.addEventListener('install', function(event) {
       })
   );
 });
-
-self.addEventListener('activate', function(event) {
-  var cacheWhitelist = ['giphyland-v2'];
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  var requestURL = new URL(event.request.url);
-
-  if (requestURL.hostname == 'api.giphy.com') {
-    event.respondWith(GiphyAPIResponse(event.request));
-  }else{
-    event.respondWith(
-      caches.match(event.request).then(function(response){
-        if(response) return response;
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(function(response){
-          var responseToCache = response.clone();
-          caches.open(CACHE_NAME)
-            .then(function(cache) {
-              cache.put(event.request, responseToCache);
-            });
-          return response;
-        });
-      })
-    );
-  }
-});
-
-function GiphyAPIResponse(request){
-  if (request.headers.get('x-use-cache-only')) {
-    return caches.match(request);
-  }else{
-    return fetch(request).then(function(response){
-      var responseToCache = response.clone();
-      caches.open(CACHE_NAME)
-        .then(function(cache) {
-          cache.put(request, responseToCache);
-        });
-      return response;
-    });
-  }
-}
 },{"serviceworker-cache-polyfill":"/Users/juliolopez/Desktop/giphyland/node_modules/serviceworker-cache-polyfill/index.js"}],"/Users/juliolopez/Desktop/giphyland/node_modules/serviceworker-cache-polyfill/index.js":[function(require,module,exports){
 if (!Cache.prototype.add) {
   Cache.prototype.add = function add(request) {
